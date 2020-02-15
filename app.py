3cd2ce9278ef824fadbae9879ba619c9d2421196
Thomas Seeber <tomseeber@Thomass-MacBook-Pro.local>
2020-02-15 13:19:17 -0500

    test

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import requests
 import json
 from config import bootcampApiUser
 from config import bootcampApiPassword
-from config import authCode
-from config import courseID
-from config import baseurl
+#from config import authCode
+#from config import courseID
+#from config import baseurl
 
 postData ={'Content-Type':'json','email': bootcampApiUser, 'password':bootcampApiPassword}
 loginRoute = 'login/'
@@ -21,25 +21,31 @@ atttendanceRoute = 'attendance/'
 #could Scrape this
 #https://bootcampspot.com/students
 #2233
-#url = 'https://bootcampspot.com/api/instructor/v1/api/instructor/v1/'
+baseurl = 'https://bootcampspot.com/api/instructor/v1/api/instructor/v1/'
 
 def bootcampDF():
     loginurl = baseurl + loginRoute
     #print(loginurl)
-    r = requests.post(url = loginurl, data = postData)
+    headers = {'Content-Type':'application/json'}
+    r = requests.post(url = loginurl, data = postData, headers=headers)
     
-    #print(r.text)
-    headers = {'Content-Type': 'application/json', 'authToken':authCode}
-    attendancePostData = {'courseId': courseID}
-    attendanceUrl = baseurl + atttendanceRoute
-    r2 = requests.post(url = attendanceUrl, data = attendancePostData, headers = headers)
+    print(r.request.headers['Content-Type'])
+    
+    #headers = {'Content-Type': 'application/json', 'authToken':authCode}
+    #attendancePostData = {'courseId': courseID}
+    #attendanceUrl = baseurl + atttendanceRoute
+    #r2 = requests.post(url = attendanceUrl, data = attendancePostData, headers = headers)
     #https://bootcampspot.com/api/instructor/v1/api/instructor/v1/attendance
     #df=pd.read_json('https://api.coinmarketcap.com/v1/ticker/?limit=10')
     #df.head()
 
-    print(r2.request.headers['Content-Type'])
-
-    print(r2.content)
+    #print(r2.request.headers['Content-Type'])
+    #print(r2.content)
     #bootcampDF = requests.get()
+
+def randomizeDF(df):
+    df = df.sample(frac=1).reset_index(drop=True)
+    #for person in df
+
 
 bootcampDF()
